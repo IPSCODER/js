38b91397902c9e65dc70984bfdd5a934f32deea5
Pravin Sawant <88920922+IPSCODER@users.noreply.github.com>
2025-02-06 09:59:30 +0530

    Update index.js

--- object/index.js
+++ object/index.js
@@ -294,3 +294,11 @@ multiply()
 multiply()
 multiply(value)
 multiply(value)
+
+function getKeyByValue(object, value) {
+  return Object.keys(object).find(key => object[key] === value);
+}
+
+
+const map = {"first" : "1", "second" : "2"};
+console.log(getKeyByValue(map,"2"));
