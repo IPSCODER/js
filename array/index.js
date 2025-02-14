b0ef9452ce5347dba0e39dec947d2c565cd16ea1
Pravin Sawant <88920922+IPSCODER@users.noreply.github.com>
2025-02-14 21:42:38 +0530

    Update index.js

--- array/index.js
+++ array/index.js
@@ -202,3 +202,64 @@ function Rearrange(array) {
 
 let arr = [1, 2, 3, -4, -1, 4];
 console.log(Rearrange(arr)); 
+
+/////////////////////////////////
+let arr = [1,2,3,4,5,6,7,8,9]
+
+for(let i = 0; i < arr.length; i++){
+    setTimeout(() =>{
+        console.log(arr[i])
+    },0)
+}
+////////////////////////////////////
+let arr = [1,2,3,4,5,6,7,8,9]
+
+for(var i = 0; i < arr.length; i++){
+    setTimeout(() =>{
+        console.log(arr[i])
+    },0)
+} output undefined undefined 9 times
+////////////////////////////////
+    let as = 0;
+for(let i = 0; i < arr.length; i++){
+    setTimeout(() =>{
+    },0)
+        as++
+    console.log(as)
+} output :1 2 3 4 5 6 7 8 9 0
+///////////////////////////////////////
+let as = 0;
+for(let i = 0; i < arr.length; i++){
+    setTimeout(() =>{
+    },0)
+        as++
+} 
+    console.log(as)
+output :9 
+///////////////////////////////////////
+for(let i = 0; i < arr.length; i++){
+    let as = 0;
+    setTimeout(() =>{
+    },0)
+        as++
+    console.log(as)
+} output 1
+/////////////////////////
+
+    let as = 0;
+for(let i = 0; i < arr.length; i++){
+    setTimeout(() =>{
+        as++
+    },0)
+    console.log(as)
+} output 0 0 0 0 0 0 0 0 0 0
+///////////////////////////
+
+    let as = 0;
+for(let i = 0; i < arr.length; i++){
+    setTimeout(() =>{
+        as++
+    },0)
+} 
+    console.log(as)
+output 0 
